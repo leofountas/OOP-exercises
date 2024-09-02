14955e4fc22588c5a014f5eda2313fe07a81d99b
leofountas <leofoutas@gmail.com>
2024-09-02 16:01:10 +0200

    second exerc

---
+++ exer.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+/* EXERCISE 1
+TODO: Create a class beverage.
+TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
+TODO: Have a default value "cold" in the construct for temperature.
+
+Remember for now we will use properties and methods that can be accessed from everywhere.
+TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
+TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
+ print the getInfo on the screen.
+TODO: Print the temperature on the screen.
+
+USE TYPEHINTING EVERYWHERE!
+*/
+
+class Beverage 
+{
+    public string $color;
+    public float $price;
+    public static string $temperature = 'cold';
+
+    public function __construct(string $color, float $price)
+    {
+        $this->color = $color;
+        $this->price = $price;
+        self::$temperature;
+    }
+
+    public function getInfo() {
+        $temperature = self::$temperature;
+        $color = $this->color;
+        echo sprintf('This beverage is %s and %s.' . '<br />', $temperature, $color);
+    }
+}
+
+
+$cola = new Beverage('black', 2.00);
+
+$cola->getInfo();
+
+
+/* EXERCISE 2
+
+TODO: Make class beer that extends from Beverage.
+TODO: Create the properties name (string) and alcoholPercentage (float).
+TODO: Foresee a construct that's allows us to use all the properties from beverage and that sets the values for name and alcoholpercentage.
+
+Remember for now we will use properties and methods that can be accessed from everywhere.
+TODO: Make a getAlcoholPercentage function which returns the alocoholPercentage.
+TODO: Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
+TODO: Also the name equal to Duvel and the alcohol percentage to 8,5%
+TODO: Print the getAlcoholPercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
+
+Make sure that each print is on a different line.
+Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
+USE TYPEHINTING EVERYWHERE!
+*/
+
+class Beer extends Beverage 
+{
+    public string $name;
+    public string $alcoholpercentage;
+
+
+    public function __construct(string $color, float $price, string $name, string $alcoholpercentage)
+    {
+        parent::__construct($color, $price);
+        $this->name = $name;
+        $this->alcoholpercentage = $alcoholpercentage;
+    }
+
+    public function getAlcoholPercentage():string {
+        return $this->alcoholpercentage;
+    }
+}
+
+
+$duvel = new Beer('blond', 3.5, 'Duvel', '8,5%');
+
+echo 'Alcohol Percentage: ' .  $duvel->getAlcoholPercentage() . '<br />';
+
+printf('Alcohol Percentage: %s' . '<br />', $duvel->getAlcoholPercentage());
+
+$duvel->getInfo();
+
+// here is the error 
+$cola->getAlcoholPercentage();
